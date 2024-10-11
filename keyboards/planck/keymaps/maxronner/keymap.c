78d4046c70c1489bb486abb6779caa752f211534
Max Ronner <max@ronner.dev>
2024-10-11 22:49:28 +0200

    fix: added exceptions for caps word

--- keyboards/planck/keymaps/maxronner/keymap.c
+++ keyboards/planck/keymaps/maxronner/keymap.c
@@ -327,6 +327,10 @@ bool caps_word_press_user(uint16_t keycode) {
         case KC_BSPC:
         case KC_DEL:
         case KC_UNDS:
+        case SE_UNDS:
+        case SE_MINS:
+        case LOWER:
+        case RAISE:
             return true;
 
         default:
