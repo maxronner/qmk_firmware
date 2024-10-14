2c317876be01fd5d69caa67435038e7549aecca6
Max Ronner <max@ronner.dev>
2024-10-14 16:35:27 +0200

    fix: removed unused leader sequence

--- keyboards/planck/keymaps/maxronner/common/leader.h
+++ keyboards/planck/keymaps/maxronner/common/leader.h
@@ -5,11 +5,9 @@ void leader_end_user(void) {
         unregister_code(KC_LSFT);
     } else if (leader_sequence_one_key(KC_L)) {
         ctrl_two_keys(KC_K, KC_C);
-    } else if (leader_sequence_one_key(KC_B)) {
-        set_single_persistent_default_layer(_COLEMAK_DH);
     } else if (leader_sequence_one_key(KC_G)) {
         set_single_persistent_default_layer(_GAMING);
     } else if (leader_sequence_one_key(KC_U)) {
         ctrl_two_keys(KC_K, KC_U);
     }
-} 
+}
