b94a081a0b88a295dce18f17374958bb64ace0a3
Max Ronner <max@ronner.dev>
2025-06-10 07:49:37 +0200

    feat(leader): Add leader-v binding for Vim configuration
    This commit introduces a new leader key sequence (Leader + v)
    that sends a series of configuration commands to Vim.
    The commands set various options including:
    - updatetime, splitright, isfname, scrolloff, signcolumn, colorcolumn
    - relative and absolute line numbers
    - tab/indentation settings (tabstop, softtabstop, shiftwidth, expandtab, smartindent)
    - nowrap
    - hlsearch, incsearch, ignorecase, smartcase

--- keyboards/planck/keymaps/maxronner/common/leader.h
+++ keyboards/planck/keymaps/maxronner/common/leader.h
@@ -32,5 +32,27 @@ void leader_end_user(void) {
     } else if (leader_sequence_two_keys(KC_Q, KC_B)) {
         // K(eyboard) S(etup) B(ootloader)
         soft_reset_keyboard();
+    } else if (leader_sequence_one_key(KC_V)) {
+        SEND_STRING(
+                    SS_TAP(X_ESC)  // Ensure Vim is in normal mode
+                    ":set updatetime=50" SS_TAP(X_ENTER)
+                    ":set splitright" SS_TAP(X_ENTER)
+                    ":set isfname+=@-@" SS_TAP(X_ENTER)
+                    ":set scrolloff=8" SS_TAP(X_ENTER)
+                    ":set signcolumn=yes" SS_TAP(X_ENTER)
+                    ":set colorcolumn=80" SS_TAP(X_ENTER)
+                    ":set relativenumber" SS_TAP(X_ENTER)
+                    ":set number" SS_TAP(X_ENTER)
+                    ":set tabstop=4" SS_TAP(X_ENTER)
+                    ":set softtabstop=4" SS_TAP(X_ENTER)
+                    ":set shiftwidth=4" SS_TAP(X_ENTER)
+                    ":set expandtab" SS_TAP(X_ENTER)
+                    ":set smartindent" SS_TAP(X_ENTER)
+                    ":set nowrap" SS_TAP(X_ENTER)
+                    ":set hlsearch" SS_TAP(X_ENTER)
+                    ":set incsearch" SS_TAP(X_ENTER)
+                    ":set ignorecase" SS_TAP(X_ENTER)
+                    ":set smartcase" SS_TAP(X_ENTER)
+                );
     }
 }
